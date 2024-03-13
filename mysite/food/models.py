12a2f4327749e4313235b36640d74af906dff6b0
Piotr Skowron <skowronpiotrmail@gmail.com>
2024-03-13 18:49:18 +0100

    Templates section

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.item_name
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    return render(request, "food/index.html", {
+        "item_list": item_list,
+    })
 
 
 def item(request):
     return HttpResponse("This is an item view")
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    return render(request, "food/detail.html", {
+        "item": item,
+    })
